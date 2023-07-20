1a7a98f34b7869727eaac43a8cc0dc06d9082714
Yegane Gholipour <yeganegholiour@gmail.com>
2023-07-20 19:30:31 +0330

    added login/logout/register

--- signup/views.py
+++ signup/views.py
@@ -6,5 +6,5 @@ from django.urls import reverse_lazy
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
-    template_name = "register/registration.html"
+    template_name = "registration/register.html"
     success_url = reverse_lazy("home")
