49021e8b873986a91c0916f6d4639c6689673448
Yegane Gholipour <yeganegholiour@gmail.com>
2023-07-20 17:33:37 +0330

    added bootstrap for base.html and rendered home.html for HomeView

--- page/views.py
+++ page/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def HomeView(request):
-    return HttpResponse("<h1>Home<h1/>")
+    return render(request, "home.html")
--- signup/urls.py
+++ signup/urls.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import RegisterView
 
-urlpatterns = [path("register/", RegisterView.as_view(), name="register")]
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+]
