8325bfd8c6d3c7b4fa7736394e2abb30f24ebac8
Yegane Gholipour <yeganegholiour@gmail.com>
2023-09-14 18:51:17 +0330

    added Profile model and default avatar

--- page/models.py
+++ page/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Category(models.Model):
@@ -22,3 +23,42 @@ class Tasks(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, default=None)
+    avatar = models.ImageField(
+        default="avatar.jpg",  # default avatar
+        upload_to="profile_avatars",  # dir to store the image
+    )
+    bio = models.TextField(default=None, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+    def save(self, *args, **kwargs):
+        """
+        ensure that the default behavior of
+          saving the model to the database is
+          not overridden by your custom save method.
+        """
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.avatar.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
+
+
+class ContactInformation(models.Model):
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    contact_type = models.CharField(
+        max_length=50, default=None, null=True, blank=True
+    )  # E.g., GitHub, Website, Email, etc.
+    contact_value = models.CharField(
+        max_length=255, default=None, null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.contact_type}: {self.contact_value}"
